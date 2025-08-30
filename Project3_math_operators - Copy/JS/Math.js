ab6530cbe5ad85ea9fa3951ba200307d8bf1086c
DevCoder244 <ty5932074@gmail.com>
2025-08-30 01:02:37 -0400

    Add files via upload

---
+++ Project3_math_operators - Copy/JS/Math.js
@@ -0,0 +1,115 @@
+//function myFunction() {
+//var sentence = "I am learning";
+//sentence += " a lot from this book";
+//  document.getElementByID("Concatenate").innerHTML = sentence;
+//}
+
+//hello(goodbye);
+
+//function hello(callback) {
+// console.log("Hello!");
+//  callback();
+//}
+
+//function wait() {
+//  console.log("Wait");
+//}
+
+//function leave() {
+// console.log("Leave");
+//}
+
+//function goodbye() {
+//  console.log("Goodbye!");
+//}
+
+//sum(displayConsole, 4, 5);
+
+//function sum(callback, x, y) {
+//  let result = x + y;
+//  callback(result);
+//}
+
+//function displayConsole(result) {
+//  console.log("The Math result is: " + result);
+//}
+
+//function minus_Function() {
+//  var minus = 15 - 7;
+//  document.getElementById("Math").innerHTML = "15 - 7 = " + minus;
+//  console.log(minus);
+//}
+//minus_Function();
+
+//function multiplication() {
+//  var simple_Math = 5 * 8;
+//  document.getElementById("Math").innnerHTML = " 5 * 8 = " + simple_Math;
+//  console.log(simple_Math);
+//}
+//multiplication();
+
+//function division() {
+//  var simple_Math = 48 / 6;
+//  document.getElementById("Math").innerHTML = "48 / 6 = " + simple_Math;
+//  console.log(simple_Math);
+//}
+
+//division();
+
+//function lotsof_Math() {
+//  var simple_Math = ((5 + 8) * 10) / 2 - 9;
+//  document.getElementById("Math").innerHTML =
+//    "5 plus 8, multiplied by 10,  divided in half and then subtracted by 9 equals " +
+//    simple_Math;
+//  console.log(simple_Math);
+//}
+//lotsof_Math();
+
+//function modulus_Operator() {
+//  var simple_Math = 40 % 37;
+//  document.getElementById("Math").innerHTML =
+//"When you divide 40 by 37 you have a remainder of: " + simple_Math;
+//console.log(simple_Math);
+//}
+//modulus_Operator();
+
+//function negation_Operator() {
+//  var x = 20;
+//  document.getElementById("Math").innerHTML = -x;
+//  console.log(-x);
+//}
+//negation_Operator();
+
+//Increment Operator//adds 1 to the variable y
+//var y = 7.35;
+//y++;
+//document.write(y);
+//console.log(y);
+
+//Decrement Operator// minus 1 from the variable d
+//var d = 10.46;
+//d--;
+//document.write(d);
+//console.log(d);
+
+//window.alert(Math.random() * 100);// will give you a random number every it is run
+
+//var x = 3.45;
+//x = Math.round(x); // 3
+//x = Math.floor(x);// 3
+//x = Math.ceil(x);//4
+//x = Math.pow(4, 9);// 262144
+//x = Math.min(4, 6, 8, 2, 1, -5, -10);// -10
+//x = Math.max(4, 6, 8, 2, 1, -5, -10);// 8
+
+//console.log(x);
+
+//let text1 = "Fancy";//This concatenates two strings
+//let text2 = "Pants";
+//let text3 = text1 + " " + text2;
+//document.getElementById("demo").innerHTML = text3;
+//console.log(text3);
+
+//let text1 = " What a very  ";//This concatenates two strings
+//text1 += "beautiful day for a swim";
+//document.getElementById("demo").innerHTML = text1;
